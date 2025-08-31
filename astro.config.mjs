015384a4c50dc5b3ba779a47e07e5b51bfb9e04f
susushi555 <143075979+susushi555@users.noreply.github.com>
2025-09-01 08:48:33 +0900

    add netlify
    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   site: 'https://sinta-byukher.github.io',
+  adapter: netlify(),
 });
